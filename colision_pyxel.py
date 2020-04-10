282d7c7c8636d8799f90c4f8bcb3183e6883e4c8
Luis Delgado <44483231+Luis-D@users.noreply.github.com>
2020-04-10 08:17:09 -0400

    Observaciones
    Quizá hayan algunos errores sintácticos aquí y allá, pero la idea de seguro se entiende.

--- colision_pyxel.py
+++ colision_pyxel.py
@@ -6,6 +6,9 @@ class App:
         pyxel.init(160, 120)
         self.jugador_x = 0
         self.jugador_y = 0
+        self.jugador_futuro_x = self.jugador_x
+        self.jugador_futuro_y = self.jugador_y
+        self.jugador_velocidad = 1
         self.jugador_h = 8
         self.jugador_w = 8
         self.bloque_x = 64
@@ -16,41 +19,59 @@ class App:
         pyxel.run(self.update, self.draw)
 
     def update(self):
-        #MOVIMIENTOS DEL JUGADOR
 
-        topEdge1 = self.jugador_y + self.jugador_h
-        rightEdge1 = self.jugador_x + self.jugador_w
-        leftEdge1 = self.jugador_x
-        bottomEdge1 = self.jugador_y
+        #ORDEN:
+         #0. Re-inicializar lo que se deba poner 'en cero' otra vez
+         #1. Entrada de datos (botones de controles)
+         #2. Actualización de 'fantasmas del futuro'
+         #3. Cálculo sobre dichos 'fantasmas'
+         #4. Actualización de las versiones reales
+         #5. Dibujado
+
+        #Si la 'rejilla' es en base a 8 (8x8), la velocidad puede ser 1,2,4,8...
+        #Esto depende del diseño de niveles, no queremos 'problemas de tunel'
+        
+        #SINCRONIZACIÓN DE FANTASMAS
+        self.jugador_futuro_x = self.jugador_x
+        self.jugador_futuro_y = self.jugador_y
+        
+        #ENTRADA DE CONTROLES:
+         if pyxel.btn(pyxel.KEY_RIGHT):
+            self.jugador_futuro_x = (self.jugador_x + self.jugador_velocidad)
+        elif pyxel.btn(pyxel.KEY_LEFT):
+            self.jugador_futuro_x = (self.jugador_x - self.jugador_velocidad)
+        elif pyxel.btn(pyxel.KEY_UP):
+            self.jugador_futuro_y = (self.jugador_y - self.jugador_velocidad)  
+        elif pyxel.btn(pyxel.KEY_DOWN):
+            self.jugador_futuro_y = (self.jugador_y + self.jugador_velocidad)
+      
+
+        #ACTUALIZACIÓN DE 'FANTASMAS'
+        topEdge1 = self.jugador_futuro_y + self.jugador_h
+        rightEdge1 = self.jugador_futuro_x + self.jugador_w
+        leftEdge1 = self.jugador_futuro_x
+        bottomEdge1 = self.jugador_futuro_y
+
+
         topEdge2 = self.bloque_y + self.bloque_h
         rightEdge2 = self.bloque_x + self.bloque_w
         leftEdge2 = self.bloque_x
         bottomEdge2 = self.bloque_y
+
+
+        #PROCESO DE COLISIÓN
+        #Lo que se hace es computar todos los objetos con todos los demás objetos (nxm).
+        #Lo que se ha de hacer es buscar alguna mejor manera de computar una cantidad indeterminada de objetos
+        
+            #detección de todas las colisiones
         collides = (leftEdge1 < rightEdge2 and rightEdge1 > leftEdge2 and bottomEdge1 < topEdge2 and topEdge1 > bottomEdge2)
-        #(rightEdge2 < leftEdge1 and leftEdge2 > rightEdge1 and topEdge2 < bottomEdge and bottomEdge2 > topEdge1)
+        
+            #respuesta de todas las colisiones
+        if not collides: #Si no se colisiona, entonces actualizar la 'versión real' del móvil
+            self.jugador_x = self.jugador_futuro_x
+            self.jugador_y = self.jugador_futuro_y
 
-        #Movimiento y colision con el otro bloque
-
-        if pyxel.btn(pyxel.KEY_RIGHT):
-            self.jugador_x = (self.jugador_x + 1) % pyxel.width
-            if(collides):
-                self.jugador_x = (self.bloque_x - self.jugador_w)
-
-        elif pyxel.btn(pyxel.KEY_LEFT):
-            self.jugador_x = (self.jugador_x - 1) % pyxel.width
-            if(collides):
-                self.jugador_x = (self.bloque_x + self.bloque_w)
-
-        elif pyxel.btn(pyxel.KEY_UP):
-            self.jugador_y = (self.jugador_y - 1) % pyxel.height
-            if(collides):
-                self.jugador_y = (self.bloque_y + self.bloque_h)
-
-
-        elif pyxel.btn(pyxel.KEY_DOWN):
-            self.jugador_y = (self.jugador_y + 1) % pyxel.height
-            if(collides):
-                self.jugador_y = (self.bloque_y - self.jugador_h)
+ 
 
 
     def draw(self):
@@ -58,30 +79,5 @@ class App:
         pyxel.cls(0)
         pyxel.rect(self.jugador_x, self.jugador_y, self.jugador_h, self.jugador_w, 9)
         pyxel.rect(self.bloque_x, self.bloque_y, self.bloque_w, self.bloque_h, 3)
-        #Simplificando esto creo?
-
-        #3 COLISIOENES incompletas
-
-
-        #1 COLISION CON LA PARED
-        #collides = (self.jugador_x <= self.bloque_x and
-        #self.jugador_x + self.jugador_w >= self.bloque_x and
-        #self.jugador_y <= self.bloque_y + self.bloque_h and
-        #self.jugador_h + self.jugador_y >= self.bloque_y)
-
-        #2 COLISIOENES
-        #collides = (self.jugador_x >= self.bloque_x
-        #and self.jugador_x <= self.bloque_x + self.bloque_w
-        #and self.jugador_y >= self.bloque_y
-        #and self.jugador_y <= self.bloque_y + self.bloque_h) or (self.jugador_x + self.jugador_w  >= self.bloque_x
-        #and self.jugador_x + self.jugador_w <= self.bloque_x + self.bloque_w
-        #and self.jugador_y + self.jugador_h >= self.bloque_y
-        #and self.jugador_y + self.jugador_h <= self.bloque_y + self.bloque_h)
-
-
-        #if(collides):
-            #pyxel.text(10, 10, "CONIAZO",15)
-
-
 
 App()
